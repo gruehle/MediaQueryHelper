73d87542f9dd87ab9940ffbb7e4f5939a69c5336
Glenn Ruehle <gruehle@gmail.com>
2012-11-16 13:47:46 -0800

    Initial commit

---
+++ MediaQueryUtils.js
@@ -0,0 +1,366 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true,  regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $, window, _rescanDoc, queriesInDocument */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    var AppInit             = brackets.getModule("utils/AppInit"),
+        Async               = brackets.getModule("utils/Async"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        FileIndexManager    = brackets.getModule("project/FileIndexManager"),
+        ProjectManager      = brackets.getModule("project/ProjectManager"),
+        StringUtils         = brackets.getModule("utils/StringUtils"),
+        TextRange           = brackets.getModule("document/TextRange").TextRange;
+    
+    var documents = [],         // CSS documents in project
+        queries = [],           // Media queries in project
+        hasUnterminated = {};   // Docs that have unterminated queries
+    
+    var mediaQueryRegEx = /^[ \t]*(@media\s+([^\{]*))\{/mg;
+    
+    var MAX_VALUE = 10000; // Arbitrarily large max width
+    
+    // MediaQuery
+    // This object represents a media query entry in a CSS file
+    function MediaQuery(doc, startLine, endLine, minValue, maxValue, queryText) {
+        var _textRange = new TextRange(doc, startLine, endLine),
+            _minValue = minValue,
+            _maxValue = maxValue,
+            _queryText = queryText;
+
+        $(_textRange).on("lostSync", function () {
+            _rescanDoc(_textRange.document);
+        });
+        
+        return {
+            textRange: _textRange,
+            minValue: _minValue,
+            maxValue: _maxValue,
+            queryText: _queryText,
+            dispose: function () {
+                _textRange.dispose();
+            },
+            matches: function (width) {
+                return width >= _minValue && width <= _maxValue;
+            },
+            closeness: function (width) {
+                return Math.min(
+                    _minValue === 0 ? MAX_VALUE : Math.abs(width - _minValue),
+                    _maxValue === MAX_VALUE ? MAX_VALUE : Math.abs(_maxValue - width)
+                );
+            }
+        };
+    }
+    
+    /**
+     * Remove all query entries for the specified document
+     */
+    function _removeDocEntries(doc) {
+        queries = queries.filter(function (query) {
+            if (query.textRange.document === doc) {
+                query.dispose();
+                return false;
+            }
+            
+            return true;
+        });
+        
+        delete hasUnterminated[doc.file.fullPath];
+    }
+    
+    /**
+     * Scan a document and extract all media queries
+     */
+    function _scanDoc(doc) {
+        // Find all matching media queries for the given CSS file's content, and add them to the
+        // overall search result
+        var text = doc.getText();
+        
+        if (text.indexOf("@media") !== -1) {
+            var match;
+            
+            while ((match = mediaQueryRegEx.exec(text)) !== null) {
+                // For now we only look for min-width and max-width 
+                var minWidthMatch = /min-width\s*\:\s*([0-9]*)/.exec(match[2]),
+                    maxWidthMatch = /max-width\s*\:\s*([0-9]*)/.exec(match[2]);
+                
+                
+                if (minWidthMatch || maxWidthMatch) {
+                    var endOffset = match.index + match[0].length + 1,
+                        braceCount = 1,
+                        startLine,
+                        endLine,
+                        minValue,
+                        maxValue;
+                    
+                    // Scan forward until we reach a matching close brace
+                    while (braceCount && endOffset < text.length) {
+                        if (text[endOffset] === "{") {
+                            braceCount++;
+                        } else if (text[endOffset] === "}") {
+                            braceCount--;
+                        }
+                        endOffset++;
+                    }
+                    
+                    // If we don't have matching braces, mark this doc as having unterminated queries
+                    // NOTE: we don't handle braces in comments, so this "unterminated" mark may be
+                    // a false positive
+                    if (braceCount > 0) {
+                        hasUnterminated[doc.file.fullPath] = true;
+                    }
+                    
+                    startLine = StringUtils.offsetToLineNum(text, match.index);
+                    endLine = StringUtils.offsetToLineNum(text, endOffset);
+                    minValue = minWidthMatch ? Number(minWidthMatch[1]) : 0;
+                    maxValue = maxWidthMatch ? Number(maxWidthMatch[1]) : MAX_VALUE; // Arbitrarily large max width
+
+                    queries.push(new MediaQuery(doc, startLine, endLine, minValue, maxValue, match[1]));
+                }
+            }
+        }
+    }
+    
+    /**
+     * Re-scan a document
+     */
+    function _rescanDoc(doc) {
+        _removeDocEntries(doc);
+        _scanDoc(doc);
+    }
+    
+    function _rangeIsInMediaQuery(doc, from, to) {
+        var i, allQueries = queriesInDocument(doc);
+        
+        for (i = 0; i < allQueries.length; i++) {
+            var query = allQueries[i],
+                startLine = query.textRange.startLine,
+                endLine = query.textRange.endLine;
+            
+            if ((from.line <= startLine && to.line >= startLine) ||
+                    (from.line <= endLine && to.line >= endLine)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    function _documentChangeHandler(event, doc, changeList) {
+        // If any of the following changes happen, rescan the document:
+        //  1. Changes to any lines that start with "@media"
+        //  2. *Any* changes in a document that have non-terminated media queries
+        //  3. Wholesale changes to the document
+        
+        var change = changeList,
+            rescanDoc = false,
+            lineNum;
+        
+        // If doc has unterminated @media queries, rescan and exit
+        if (hasUnterminated[doc.file.fullPath]) {
+            // console.log("doc has unterminated queries");
+            _rescanDoc(doc);
+            return;
+        }
+        
+        while (change) {
+            // If from && to are undefined, the change is wholesale
+            if (!change.from && !change.to) {
+                rescanDoc = true;
+                break;
+            }
+            
+            // If the change is in the first or last line of a media query, rescan
+            if (_rangeIsInMediaQuery(doc, change.from, change.to)) {
+                // console.log("change modified a media query");
+                rescanDoc = true;
+                break;
+            }
+            
+            var text = doc.getRange(
+                {line: change.from.line, ch: 0},
+                {line: change.to.line + 1, ch: 0}
+            );
+            
+            // If the changed range has "@media", rescan
+            if (text.search(mediaQueryRegEx) !== -1) {
+                // console.log("changed text line has @media");
+                rescanDoc = true;
+                break;
+            }
+            
+            // If the new text has a media query, rescan
+            if (change.text.join("\n").search(mediaQueryRegEx) !== -1) {
+                // console.log("new text has media query");
+                rescanDoc = true;
+                break;
+            }
+            
+            change = change.next;
+        }
+        
+        if (rescanDoc) {
+            // console.log("rescanning for media queries: " + doc.file.fullPath);
+            _rescanDoc(doc);
+        }
+    }
+    
+    function _documentDeletedHandler(event) {
+        var deletedDoc = event.target;
+        
+        _removeDocEntries(deletedDoc);
+        documents = documents.filter(function (doc) {
+            return deletedDoc !== doc;
+        });
+    }
+    
+    // Called when a new project is opened
+    function _onProjectOpen() {
+        function _loadFileAndScan(fullPath) {
+            var oneFileResult = new $.Deferred();
+            
+            DocumentManager.getDocumentForPath(fullPath)
+                .done(function (doc) {
+                    $(doc).on("change", _documentChangeHandler);
+                    $(doc).on("deleted", _documentDeletedHandler);
+                    doc.addRef();
+                    documents.push(doc);
+                    _scanDoc(doc);
+                    oneFileResult.resolve();
+                })
+                .fail(function (error) {
+                    oneFileResult.reject(error);
+                });
+        
+            return oneFileResult.promise();
+        }
+        
+        // Remove old doc change handlers and references
+        documents.forEach(function (doc) {
+            $(doc).off("change", _documentChangeHandler);
+            $(doc).off("deleted", _documentDeletedHandler);
+            // doc.releaseRef();
+        });
+        
+        // Release old queries
+        queries.forEach(function (query) {
+            query.dispose();
+        });
+        queries = [];
+        hasUnterminated = {};
+        
+        FileIndexManager.getFileInfoList("css").done(function (fileInfos) {
+            Async.doInParallel(fileInfos, function (fileInfo, number) {
+                return _loadFileAndScan(fileInfo.fullPath);
+            });
+        });
+    }
+    
+    /**
+     * Return a list of all media queries in the project that match
+     * the specified width.
+     * @param {number} width Width to use for matching queries
+     * @return {Array.<MediaQuery>} Queries that match the specified width. Returns
+     *          an emtpy array if there are no matches in the project.
+     */
+    function findAllMatches(width) {
+        var matches = [];
+        
+        queries.forEach(function (query) {
+            if (query.matches(width)) {
+                matches.push(query);
+            }
+        });
+        
+        return matches;
+    }
+    
+    /**
+     * Finds the media query that has the closest match to the specified width.
+     * @param {number} width Width to use for matching queries
+     * @return {MediaQuery} Query with the closest match. Returns NULL if no
+     *        matches are found.
+     */
+    function findClosestMatch(width) {
+        var allMatches = findAllMatches(width), closest;
+        
+        allMatches.forEach(function (query) {
+            if (!closest) {
+                closest = query;
+            } else {
+                if (query.closeness(width) < closest.closeness(width)) {
+                    closest = query;
+                }
+            }
+        });
+        
+        return closest;
+    }
+    
+    /**
+     * Returns the media query at the specified document position
+     * @param {string} fullPath Full path of the document
+     * @param {{line: number, ch:number}} pos Position within document
+     * @return {MediaQuery} Media query at the specified position. Returns
+     *       NULL if there are no queries at the specified position.
+     */
+    function queryAtDocumentPosition(fullPath, pos) {
+        var i;
+        
+        for (i = 0; i < queries.length; i++) {
+            var query = queries[i];
+            if (query.textRange.document.file.fullPath === fullPath) {
+                if (pos.line >= query.textRange.startLine &&
+                        pos.line <= query.textRange.endLine) {
+                    return query;
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    function queriesInDocument(doc) {
+        var result = [];
+        
+        queries.forEach(function (query) {
+            if (query.textRange.document.file.fullPath === doc.file.fullPath) {
+                result.push(query);
+            }
+        });
+        
+        return result;
+    }
+    
+    $(ProjectManager).on("projectOpen", _onProjectOpen);
+    // "projectOpen" isn't sent for initial project, load on
+    // appReady event
+    AppInit.appReady(_onProjectOpen);
+    
+    exports.findAllMatches = findAllMatches;
+    exports.findClosestMatch = findClosestMatch;
+    exports.queryAtDocumentPosition = queryAtDocumentPosition;
+    exports.queriesInDocument = queriesInDocument;
+});
---
+++ main.js
@@ -0,0 +1,273 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true,  regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $, window, PathUtils */
+
+// TODO:
+//  initial preview file
+//  make sure extra work is only done when extension is active
+//  unit tests
+//  disable iframe clicks/focus
+//  enabled state prefs
+//  localize
+//  ---
+//  add dropdown to anchor, list matches in closeness order
+//  handle comments when parsing for media queries - need test cases first
+//  be more robust when loading iframe content - need test cases
+// DONE:
+//  X horizontal scroll
+//  X Support min-width and max-width
+//  X Layout of scaled iframe
+//  X status bar
+//  X load sizes from config file
+//  X query object
+//  X CSS editing
+//   X edit range of existing query
+//   X obliterate query
+//   X add new query
+//   X edit query params
+//   X undo for the above
+//
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    // Brackets modules
+    var AppInit             = brackets.getModule("utils/AppInit"),
+        CommandManager      = brackets.getModule("command/CommandManager"),
+        Commands            = brackets.getModule("command/Commands"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        EditorManager       = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),
+        Inspector           = brackets.getModule("LiveDevelopment/Inspector/Inspector"),
+        Menus               = brackets.getModule("command/Menus"),
+        ProjectManager      = brackets.getModule("project/ProjectManager"),
+        RemoteAgent         = brackets.getModule("LiveDevelopment/Agents/RemoteAgent");
+    
+    // Extension modules
+    var MediaQueryModel     = require("MediaQueryUtils");
+    
+    // Templates
+    var config              = JSON.parse(require("text!preview-config.json"));
+    
+    // Preview file (relative to project root)
+    var previewFile = "index.html";
+    var previewFileFound = false;
+    
+    // Status message holder
+    var $status = $("<div id='media-query-status'>");
+    
+    // Main preview holder
+    var $preview = $("<div id='media-query-preview'>");
+
+    // Create the preview thumbnails
+    var PADDING = 20;
+    var xPos = PADDING / 2;
+    config.sizes.forEach(function (size) {
+        var scaledWidth = Math.round(size * config.scale);
+        
+        var $iframe = $("<iframe>")
+            .attr({
+                width: size,
+                height: 600 * 0.3 / config.scale,
+                sandbox: "allow-scripts allow-same-origin",
+                seamless: true,
+                "tab-index": -1
+            })
+            .css({
+                "-webkit-transform": "scale(" + config.scale + ")"
+            });
+        
+        var $thumbnail = $("<div class='preview-holder'>")
+            .css({
+                left: xPos,
+                width: scaledWidth
+            })
+            .append($iframe)
+            .append($("<a class='size-link' href='#'>" + size + "px</a>"));
+        
+        $preview.append($thumbnail);
+        
+        xPos += (scaledWidth + PADDING);
+    });
+    
+    // Add label click handlers to find and open the nearest match
+    $preview.find("a").click(function (e) {
+        var width = /[0-9]*/.exec(e.target.innerText)[0];
+        
+        var query = MediaQueryModel.findClosestMatch(width);
+        
+        if (query) {
+            CommandManager.execute(Commands.FILE_OPEN, {fullPath: query.textRange.document.file.fullPath})
+                .done(function (doc) {
+                    // Opened document is now the current main editor
+                    EditorManager.getCurrentFullEditor().setSelection(
+                        {line: query.textRange.startLine, ch: 0},
+                        {line: query.textRange.endLine + 1, ch: 0}
+                    );
+                    
+                    // Highlight the doc in the project tree
+                    CommandManager.execute(Commands.NAVIGATE_SHOW_IN_FILE_TREE);
+                });
+        }
+    });
+    
+    // Add preview to the main toolbar
+    $preview.hide().appendTo("#main-toolbar");
+
+    // Highlight thumbnail for media selector being edited
+    var _editor; // Editor whose cursor we are tracking
+    
+    function indicateActiveMediaQuery() {
+        // Remove all existing highlights and status
+        $("#media-query-preview iframe").attr("class", "");
+        $status.text("");
+        
+        if (!_editor) {
+            return;
+        }
+        
+        var query = MediaQueryModel.queryAtDocumentPosition(
+            _editor.document.file.fullPath,
+            _editor._codeMirror.getCursor(true)  // Cant use Editor.getCursorPos() because we want the start pos
+        );
+        
+        if (query) {
+            config.sizes.forEach(function (size) {
+                $("iframe[width=" + size + "]").addClass(query.matches(size) ? "highlighted" : "dimmed");
+            });
+            
+            $status.text(query.queryText);
+        }
+    }
+    
+    function currentDocumentChange() {
+        if (_editor) {
+            $(_editor).off("cursorActivity", indicateActiveMediaQuery);
+            _editor = null;
+        }
+        
+        // Remove any existing highlighting
+        indicateActiveMediaQuery();
+        
+        var newDoc = DocumentManager.getCurrentDocument();
+        if (newDoc && PathUtils.filenameExtension(newDoc.file.fullPath).search(/css/i) !== -1) {
+            _editor = EditorManager.getCurrentFullEditor();
+            $(_editor).on("cursorActivity", indicateActiveMediaQuery);
+        }
+    }
+    
+    // State properties
+    var enabled = false;
+    
+    // Load the preview document into the iframes. For now we look for "index.html"
+    // in the root of the project directory. We will need to do something smarter
+    // than this...
+    function loadContent() {
+        if (enabled) {
+            var previewDocumentPath = ProjectManager.getProjectRoot().fullPath + previewFile;
+            
+            $preview.find(".no-preview-file").remove();
+            
+            if (previewFileFound) {
+                $preview.find("iframe").attr("src", previewDocumentPath);
+            } else {
+                brackets.fs.stat(previewDocumentPath, function (err) {
+                    if (!err) {
+                        previewFileFound = true;
+                        loadContent();
+                    } else {
+                        $preview.find("iframe").attr("src", "");
+                        
+                        /*
+                        var $fileSelector = $("<div class='no-preview-file'></div>")
+                            .append($("<div class='alert-message'>Enter a file to preview</div>"))
+                            .append($("<div><input prompt='index.html'></input></div>"))
+                            .appendTo($preview);
+                        */
+                        
+                        
+                        var $message = $("<div class='no-preview-file alert-message'>")
+                            .css({
+                                top: 100,
+                                width: 450,
+                                margin: "auto"
+                            })
+                            .html("Oops! I can't find an <span class='dialog-filename'>index.html</span> file at the root of your project.")
+                            .appendTo($preview);
+                        
+                    }
+                });
+            }
+        }
+    }
+    
+    // Load our stylesheet
+    ExtensionUtils.loadStyleSheet(module, "MediaQueryHelper.css");
+    
+    // Add menu command
+    var ENABLE_MEDIA_QUERY_PREVIEW      = "Enable Media Query Utilities";
+    var CMD_ENABLE_MEDIA_QUERY_PREVIEW  = "utils.enableMediaQueryUtils";
+
+    function updateMenuItemCheckmark() {
+        CommandManager.get(CMD_ENABLE_MEDIA_QUERY_PREVIEW).setChecked(enabled);
+    }
+
+    function toggleEnableMediaQueryUtils() {
+        enabled = !enabled;
+        if (enabled) {
+            loadContent();
+            $preview.show();
+        } else {
+            $preview.hide();
+        }
+        EditorManager.resizeEditor();
+        updateMenuItemCheckmark();
+    }
+    
+    // Register command and add menu item
+    CommandManager.register(ENABLE_MEDIA_QUERY_PREVIEW, CMD_ENABLE_MEDIA_QUERY_PREVIEW, toggleEnableMediaQueryUtils);
+    var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+    menu.addMenuItem(CMD_ENABLE_MEDIA_QUERY_PREVIEW);
+    updateMenuItemCheckmark();
+    
+    // Toolbar icon
+    var $icon = $("<a id='media-query-preview-icon' href='#'></a>")
+        .click(toggleEnableMediaQueryUtils)
+        .appendTo($("#main-toolbar .buttons"));
+    
+    // Status indicator
+    $("#status-info").append($status);
+    
+    // Refresh preview content on startup, when a document is saved, and on project open
+    AppInit.appReady(loadContent);
+    $(DocumentManager).on("documentSaved", loadContent);
+    $(ProjectManager).on("projectOpen", function () {
+        previewFileFound = false;
+        loadContent();
+    });
+
+    // Listen for document change to setup active media query tracking
+    AppInit.appReady(currentDocumentChange);
+    $(DocumentManager).on("currentDocumentChange", currentDocumentChange);
+});
